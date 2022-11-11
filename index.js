66380750cdda49140a9350643a5d6a2909eb0ff5
sbswag9@gmail.com <sbswag9@gmail.com>
2022-11-11 22:12:02 +0600

    setup mongodb

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-const port = 3000;
+
+const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8jr6sk9.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const serviceCollection = client.db("reddy_customs").collection("services");
+  } finally {
+  }
+}
+
+run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("reddy_customs server is running");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`reddy_customs listening on port ${port}`);
 });
